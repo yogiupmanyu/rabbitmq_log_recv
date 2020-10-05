ef2e23f23fc340c3c1ddd337b15b99b12e781d99
Yogesh <yogiupmanyu@gmail.com>
2020-10-05 15:58:00 +0530

    first commit.. from test_branch

--- src/main/java/com/altran/rabbitmq_log_recv/RabbitmqLogRecvApplication.java
+++ src/main/java/com/altran/rabbitmq_log_recv/RabbitmqLogRecvApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqLogRecvApplication {
 
+	// commit in test_branch
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqLogRecvApplication.class, args);
 	}
